33cef4027a7fddd07bf48b17264d9957f8470017
DinakarCiber <dnaramala@ciber.com>
2020-09-22 15:29:36 +0530

    changes for seach Results

--- src/app/components/request/request.component.ts
+++ src/app/components/request/request.component.ts
@@ -8,8 +8,18 @@ import { Component, OnInit } from '@angular/core';
 export class RequestComponent implements OnInit {
 
   constructor() { }
-
+  searchBar = false;
+  create_request = true;
   ngOnInit(): void {
   }
-
+  loadSearchRequest() {
+    this.searchBar = !this.searchBar;
+    this.create_request = !this.create_request;
+    console.log("test"+this.create_request+" bar"+this.searchBar)
+  }
+  loadCreateRequest() {
+    this.searchBar = !this.searchBar;
+    this.create_request = !this.create_request;
+  }
+  createRequest() { }
 }
