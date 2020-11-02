f37ce418ecd340bb32ec9a7edfd9193ef29355ca
DinakarCiber <dnaramala@ciber.com>
2020-11-02 13:11:24 +0530

    changes for UserRoles

--- src/app/components/user-roles/user-roles.component.ts
+++ src/app/components/user-roles/user-roles.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { TreeNode } from 'primeng/api';
+import { EMPTY } from 'rxjs';
+import { UserRolesService } from 'src/app/services/userRoles/user-roles.service';
 @Component({
   selector: 'app-user-roles',
   templateUrl: './user-roles.component.html',
@@ -7,9 +9,487 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserRolesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userRolesService: UserRolesService) { }
 
   ngOnInit(): void {
   }
+  roleId: string;
+  searchNetworkId: string;
+  userEmailId: string;
+  userRoleList: string[];
+  showUserSearchView = true;
+  showSearchResultsDiv = false;
+  errorMessageforSearchDiv = false;
+  deletedSuccessMsgDiv = false;
+  errorMessageDiv = false;
+  createUserDiv = false;
+  createErrorMessageDiv = false;
+  createEmailMessageDiv = false;
+  createSuccussMessageDiv = false;
+  searchRowResultsDiv = false;
+  editusersDiv = false;
+  deleteUserDiv = false;
+  searchShowResultsDiv = false;
+  searchResultsDiv = false;
+  totalRecordsCountDiv = false;
+  userRolePaginationDiv = false;
+  userRoleFormVal: TreeNode[];
+  userRolesVal: TreeNode[];
+  userRoleListVal: TreeNode[];
+  userResponse: TreeNode[];
 
+  lastbutton = true;
+  firstbutton = true;
+  previousbutton = true;
+  nextbutton = true;
+
+  searchResultContainerDiv = false;
+  pageNumber: number;
+  institutionId: number;
+  institutionIdErrMsgDiv = false;
+  roleIdErrMsgDiv = false;
+  //edit User
+  editEmailId: string;
+  edituserDescription: string;
+  editnetworkLoginId: string;
+  editinstitutionId: string;
+  editroleId: string[];
+  editsuccessMsgDiv = false;
+  editerrormsgDiv = false;
+  editinstitutionIdErrMsgDiv = false;
+  editroleIdErrMsgDiv = false;
+  //delete User
+  deleteNetworkLoginId: string;
+  deleteInstitutionId: string;
+  deletedRoleId: string[];
+  deleteUserDescription: string;
+  deleteEmailId: string;
+  deleteErrorMsgDiv = false;
+
+  numOfRecordsId = 10;
+  //create User
+  emailId: string;
+  userDescription: string;
+  rolesOption: string;
+  //institutionId : string;
+  networkLoginId: string;
+  selectedForCreate: number[];
+  editUserId: number;
+  numOfRecordsId: number;
+  userId: number;
+  searchBarDiv = true;
+  postData = {
+    "userId": null,
+    "institutionId": null,
+    "editUserId": 0,
+    "roleId": null,
+    "editRoleId": null,
+
+    "pageNumber": 0,
+    "pageSize": 10,
+    "totalPageCount": 0,
+    "editInstitutionId": null,
+    "afterDelPageNumber": 0,
+    "afterDelPageSize": 10,
+    "afterDelTotalPageCount": 0,
+
+    "searchNetworkId": null,
+    "networkLoginId": null,
+    "roleName": null,
+    "totalRecordsCount": "0",
+    "institutionName": null,
+    "message": null,
+    "errorMessage": null,
+    "errorMessageForEmail": null,
+    "editErromessage": null,
+    "sectionName": null,
+    "buttonName": null,
+    "editNetworkId": null,
+    "userDescriptionErrMsg": null,
+    "userDescription": null,
+    "editNetworkLoginId": null,
+    "editUserDescription": null,
+    "userEmailId": null,
+    "emailId": null,
+    "editEmailId": null,
+
+    "allowCreateEdit": null,
+    "isCreatedRequest": null,
+    "showPagination": false,
+    "showSearch": false,
+    "showErrorMessage": false,
+    "showCreateSuccess": false,
+    "showCreateError": false,
+    "showEditSuccess": false,
+    "showEditError": false,
+    "showCreateEmailError": false,
+    "deleteSuccessMsg": false,
+    "selected": null,
+    "submitted": null,
+    "showResults": false,
+    "deletedSuccessMsg": false,
+    "deleteErrorMsg": false,
+    "showUserSearchView": true,
+
+    "roles": [],
+    "institutions": [],
+    "showSelectedForCreate": [],
+    "selectedForCreate": [],
+    "editSelectedForCreate": [],
+    "userRoleFormList": [],
+
+    "showEditDeleteIcon": true,
+
+    "createdBy": null,
+    "lastUpdatedBy": null
+  }
+  enableCreateUser() {
+    this.userRoles();
+    this.createUserDiv = true;
+    this.errorMessageDiv = false;
+    this.showUserSearchView = false;
+    this.deleteUserDiv = false;
+  }
+  editUser(userId, networkLoginId, roleName) {
+    this.editusersDiv = true;
+    this.createUserDiv = false;
+    this.showSearchResultsDiv = false;
+    this.errorMessageDiv = false;
+    this.searchResultsDiv = false;
+    this.deleteUserDiv = false;
+    this.showUserSearchView = false;
+    this.editsuccessMsgDiv = false;
+    this.editerrormsgDiv = false;
+    this.userId = userId;
+    this.userRolesService.editUser(userId, networkLoginId).subscribe(
+      (res) => {
+        this.userResponse = res;
+        this.userRoleListVal = this.userResponse['roles'];
+        this.editEmailId = this.userResponse['editEmailId'];
+        this.edituserDescription = this.userResponse['editUserDescription'];
+        this.editnetworkLoginId = this.userResponse['editNetworkLoginId'];
+        this.editinstitutionId = this.userResponse['editInstitutionId'];
+        this.editroleId = roleName;
+        console.log("roleName", roleName);
+
+      });
+  }
+  deleteUserRole(userId, networkLoginId, roleName) {
+    this.editusersDiv = false;
+    this.createUserDiv = false;
+    this.showSearchResultsDiv = false;
+    this.errorMessageDiv = false;
+    this.searchResultsDiv = false;
+    this.deleteUserDiv = true;
+    this.showUserSearchView = false;
+    this.editsuccessMsgDiv = false;
+    this.editerrormsgDiv = false;
+    this.userId = userId;
+    //this.deletePageNumber = 
+    this.userRolesService.editUser(userId, networkLoginId).subscribe(
+      (res) => {
+        this.userResponse = res;
+        this.userRoleListVal = this.userResponse['roles'];
+        this.deleteEmailId = this.userResponse['editEmailId'];
+        this.deleteUserDescription = this.userResponse['editUserDescription'];
+        this.deleteNetworkLoginId = this.userResponse['editNetworkLoginId'];
+        this.deleteInstitutionId = this.userResponse['editInstitutionId'];
+        this.deletedRoleId = roleName;
+      });
+  }
+  searchUserRoles() {
+    this.userRolesService.searchRoles(this.setPostData('searchUsers')).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        if (this.userRoleFormVal['message'] != null) {
+          this.searchResultsDiv = true;
+          this.searchResultContainerDiv = true;
+          this.errorMessageforSearchDiv = true;
+          this.showSearchResultsDiv = false;
+          this.searchRowResultsDiv = false;
+          this.totalRecordsCountDiv = false;
+          this.userRolePaginationDiv = false;
+          console.log(this.userRoleFormVal['message']);
+        } else if (this.userRoleFormVal['errorMessage'] != null) {
+          this.errorMessageforSearchDiv = false;
+        } else {
+          this.pagination();
+          this.searchResultContainerDiv = true;
+          this.searchResultsDiv = true;
+          this.showSearchResultsDiv = true;
+          this.errorMessageDiv = false;
+          this.showUserSearchView = true;
+          this.errorMessageforSearchDiv = false;
+          this.deletedSuccessMsgDiv = false;
+          this.searchShowResultsDiv = true;
+          this.searchRowResultsDiv = true;
+          this.totalRecordsCountDiv = true;
+          this.userRolePaginationDiv = true;
+        }
+
+      }
+    );
+  }
+  showEntriesChange(pageSize) {
+    this.numOfRecordsId = pageSize;
+    this.userRolesService.pageSize(this.setPostData('pageSize')).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        this.pagination();
+      });
+  }
+  createUser(emailId, userDescription, roleOptions, institutionId, networkLoginId) {
+
+    this.emailId = emailId;
+    this.userDescription = userDescription;
+    this.selectedForCreate = [1];
+    this.institutionId = institutionId;
+    this.networkLoginId = networkLoginId;
+
+    this.userRolesService.createUser(this.setPostData('createUser')).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        this.createUserDiv = true;
+        this.showSearchResultsDiv = false;
+        this.errorMessageDiv = false;
+        this.searchResultsDiv = false;
+        this.deleteUserDiv = false;
+        this.showUserSearchView = false;
+        this.editsuccessMsgDiv = false;
+        this.editerrormsgDiv = false;
+        if (this.userRoleFormVal['message'] != null) {
+          this.createSuccussMessageDiv = true;
+          this.createErrorMessageDiv = false;
+        } else if (this.userRoleFormVal['errorMessage'] != null) {
+          this.createSuccussMessageDiv = false;
+          this.createErrorMessageDiv = true;
+        }
+        this.emailId = '';
+        this.userDescription = '';
+        this.rolesOption = '';
+        this.institutionId = null;
+        this.networkLoginId = '';
+      });
+  }
+  saveEditUser(roleIds, networkLoginId, userDescription, institutionId, userEmailId) {
+    this.userRolesService.saveEditUser(this.userId, 1, networkLoginId, userDescription, institutionId, userEmailId).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        this.editusersDiv = true;
+        this.createUserDiv = false;
+        this.showSearchResultsDiv = false;
+        this.errorMessageDiv = false;
+        this.searchResultsDiv = false;
+        this.deleteUserDiv = false;
+        this.showUserSearchView = false;
+        this.editsuccessMsgDiv = false;
+        this.editerrormsgDiv = false;
+        if (this.userRoleFormVal['message'] != null) {
+          this.editsuccessMsgDiv = true;
+          this.editerrormsgDiv = false;
+        } else if (this.userRoleFormVal['errorMessage'] != null) {
+          this.editsuccessMsgDiv = false;
+          this.editerrormsgDiv = true;
+        }
+      });
+  }
+  deleteUser(networkLoginId) {
+    this.userRolesService.delete(this.userId, networkLoginId, this.userRoleFormVal['pageNumber'], this.userRoleFormVal['totalPageCount'], this.userRoleFormVal['pageSize']).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        if (this.userRoleFormVal['message'] != null) {
+          this.deletedSuccessMsgDiv = true;
+          this.deleteUserDiv = false;
+          this.deleteErrorMsgDiv = false;
+          this.searchBarDiv = true;
+          this.searchResultContainerDiv = true;
+          this.searchResultsDiv = true;
+          this.showSearchResultsDiv = true;
+          this.searchShowResultsDiv = true;
+          this.showUserSearchView = true;
+          this.errorMessageDiv = false;
+          this.errorMessageforSearchDiv = false;
+          console.log(this.userRoleFormVal['message']);
+        } else if (this.userRoleFormVal['errorMessage'] != null) {
+          this.deletedSuccessMsgDiv = false;
+          this.deleteErrorMsgDiv = true;
+        }
+      });
+  }
+  userRoles() {
+    this.userRolesService.userRoles().subscribe(
+      (res) => {
+        this.userRolesVal = res;
+        this.userRoleListVal = this.userRolesVal['roles'];
+        this.userRoleList = this.userRoleListVal[''];
+      });
+  }
+  lastCall() {
+    this.userRolesService.lastCall(this.setPostData('lastCall')).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        this.pagination();
+      });
+  }
+  previousCall() {
+    this.userRolesService.previousCall(this.setPostData('previousCall')).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        this.pagination();
+      });
+  }
+  nextCall() {
+    this.userRolesService.nextCall(this.setPostData('nextCall')).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        this.pagination();
+      });
+  }
+  firstCall() {
+    this.userRolesService.firstCall(this.setPostData('firstCall')).subscribe(
+      (res) => {
+        this.userRoleFormVal = res;
+        this.pagination();
+      });
+  }
+  toggleUserInstitutionValidation() {
+    if (this.institutionId == null || this.institutionId == undefined) {
+      this.institutionIdErrMsgDiv = true;
+    }
+  }
+  pagination() {
+    if (this.userRoleFormVal['pageNumber'] == 0 && (this.userRoleFormVal['totalPageCount'] - 1 > 0)) {
+      this.firstbutton = true;
+      this.previousbutton = true;
+      this.nextbutton = false;
+      this.lastbutton = false;
+    } else if (this.userRoleFormVal['pageNumber'] == 0 && (this.userRoleFormVal['pageNumber'] == this.userRoleFormVal['totalPageCount'] - 1)) {
+      this.firstbutton = true;
+      this.previousbutton = true;
+      this.nextbutton = true;
+      this.lastbutton = true;
+    }
+    else if ((this.userRoleFormVal['pageNumber'] == this.userRoleFormVal['totalPageCount'] - 1) && this.userRoleFormVal['totalPageCount'] - 1 > 0) {
+      this.firstbutton = false;
+      this.previousbutton = false;
+      this.nextbutton = true;
+      this.lastbutton = true;
+    } else if ((this.userRoleFormVal['pageNumber'] < this.userRoleFormVal['totalPageCount'] - 1) && (this.userRoleFormVal['pageNumber'] != 0)) {
+      this.firstbutton = false;
+      this.previousbutton = false;
+      this.nextbutton = false;
+      this.lastbutton = false;
+    } else if (this.userRoleFormVal['pageNumber'] == 0 && this.userRoleFormVal['totalPageCount'] == 0) {
+      this.firstbutton = true;
+      this.previousbutton = true;
+      this.nextbutton = true;
+      this.lastbutton = true;
+    }
+  }
+  setPostData(actionName) {
+    if (actionName == 'createUser') {
+      this.resetFields();
+    } else if (actionName == 'searchUsers') {
+      this.numOfRecordsId = 10;
+      this.pageNumber = 0;
+      this.validateInput();
+    } else if (actionName == 'firstCall') {
+      this.validateInput();
+      this.pageNumber = 0;
+    } else if (actionName == 'lastCall') {
+      this.validateInput();
+      this.pageNumber = this.userRoleFormVal['totalPageCount'];
+      console.log("pageNumber", this.pageNumber);
+    } else if (actionName == 'previousCall') {
+      this.validateInput();
+      this.pageNumber = this.userRoleFormVal['pageNumber'];
+    } else if (actionName == 'nextCall') {
+      this.validateInput();
+      this.pageNumber = this.userRoleFormVal['pageNumber'];
+    } else if (actionName == 'pageSize') {
+      this.validateInput();
+    }
+
+    this.postData = {
+      "userId": null,
+      "institutionId": this.institutionId,
+      "editUserId": 0,
+      "roleId": null,
+      "editRoleId": null,
+
+      "pageNumber": this.pageNumber,
+      "pageSize": this.numOfRecordsId,
+      "totalPageCount": 0,
+      "editInstitutionId": null,
+      "afterDelPageNumber": 0,
+      "afterDelPageSize": 10,
+      "afterDelTotalPageCount": 0,
+
+      "searchNetworkId": this.searchNetworkId,
+      "networkLoginId": this.networkLoginId,
+      "roleName": null,
+      "totalRecordsCount": "0",
+      "institutionName": null,
+      "message": null,
+      "errorMessage": null,
+      "errorMessageForEmail": null,
+      "editErromessage": null,
+      "sectionName": null,
+      "buttonName": null,
+      "editNetworkId": null,
+      "userDescriptionErrMsg": null,
+      "userDescription": this.userDescription,
+      "editNetworkLoginId": null,
+      "editUserDescription": null,
+      "userEmailId": this.userEmailId,
+      "emailId": this.emailId,
+      "editEmailId": null,
+
+      "allowCreateEdit": null,
+      "isCreatedRequest": null,
+      "showPagination": false,
+      "showSearch": false,
+      "showErrorMessage": false,
+      "showCreateSuccess": false,
+      "showCreateError": false,
+      "showEditSuccess": false,
+      "showEditError": false,
+      "showCreateEmailError": false,
+      "deleteSuccessMsg": false,
+      "selected": null,
+      "submitted": null,
+      "showResults": false,
+      "deletedSuccessMsg": false,
+      "deleteErrorMsg": false,
+      "showUserSearchView": true,
+
+      "roles": [],
+      "institutions": [],
+      "showSelectedForCreate": [],
+      "selectedForCreate": [],
+      "editSelectedForCreate": [],
+      "userRoleFormList": [],
+
+      "showEditDeleteIcon": true,
+
+      "createdBy": null,
+      "lastUpdatedBy": null
+    }
+    return this.postData;
+  }
+  resetFields() {
+    this.searchNetworkId = "";
+    this.userEmailId = "";
+  }
+  validateInput() {
+    if (this.userEmailId == null && this.searchNetworkId == null) {
+      this.userEmailId = "";
+      this.searchNetworkId = "";
+    } else if (this.userEmailId == null) {
+      this.userEmailId = "";
+    } else if (this.searchNetworkId == null) {
+      this.searchNetworkId = "";
+    }
+  }
 }
+
--- src/app/services/userRoles/user-roles.service.ts
+++ src/app/services/userRoles/user-roles.service.ts
@@ -1,9 +1,97 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { TreeNode } from 'primeng/api';
+import { from, Observable } from 'rxjs';
+import { appHeaders } from 'src/config/headers';
+import { urls } from 'src/config/urls';
+import { UserRoleFormData } from 'src/app/model/UserRoleFormData';
 @Injectable({
   providedIn: 'root'
 })
 export class UserRolesService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+  baseUrl = urls.baseUrl;
+  prefix = urls.userRoles;
+
+  searchRoles(postData: UserRoleFormData): Observable<TreeNode[]> {
+    return this.httpClient.post<TreeNode[]>(this.baseUrl + this.prefix + "/searchUsers", postData,
+      {
+        headers: appHeaders.getHeaders()
+      });
+  }
+  previousCall(postData: UserRoleFormData): Observable<TreeNode[]> {
+    return this.httpClient.post<TreeNode[]>(this.baseUrl + this.prefix + "/previous", postData,
+      {
+        headers: appHeaders.getHeaders()
+      });
+  }
+  nextCall(postData: UserRoleFormData): Observable<TreeNode[]> {
+    return this.httpClient.post<TreeNode[]>(this.baseUrl + this.prefix + "/next", postData,
+      {
+        headers: appHeaders.getHeaders()
+      });
+  }
+  firstCall(postData: UserRoleFormData): Observable<TreeNode[]> {
+    return this.httpClient.post<TreeNode[]>(this.baseUrl + this.prefix + "/first", postData,
+      {
+        headers: appHeaders.getHeaders()
+      });
+  }
+  lastCall(postData: UserRoleFormData): Observable<TreeNode[]> {
+    return this.httpClient.post<TreeNode[]>(this.baseUrl + this.prefix + "/last", postData,
+      {
+        headers: appHeaders.getHeaders()
+      });
+  }
+  pageSize(postData: UserRoleFormData): Observable<TreeNode[]> {
+    return this.httpClient.post<TreeNode[]>(this.baseUrl + this.prefix + "/searchUsers", postData,
+      {
+        headers: appHeaders.getHeaders()
+      });
+  }
+  userRoles(): Observable<TreeNode[]> {
+    return this.httpClient.get<TreeNode[]>(this.baseUrl + this.prefix + "/userRoles",
+      {
+        headers: appHeaders.getHeaders()
+      });
+  }
+  editUser(userId,networkLoginId): Observable<TreeNode[]> {
+    let headers = appHeaders.getHeaders();
+    let parames = new HttpParams()
+      .set('userId', userId)
+      .set('networkLoginId', networkLoginId);
+    const options = { params: parames, headers: headers };
+    return this.httpClient.get<TreeNode[]>(this.baseUrl + this.prefix + "/editUser",options);
+  }
+  saveEditUser(userId,roleIds,networkLoginId,userDescription,institutionId,userEmailId): Observable<TreeNode[]> {
+    let headers = appHeaders.getHeaders();
+    let parames = new HttpParams()
+      .set('userId', userId)
+      .set('networkLoginId', networkLoginId)
+      .set('userDescription',userDescription)
+      .set('institutionId',institutionId)
+      .set('userEmailId',userEmailId)
+      .set('roleIds',roleIds);
+    const options = { params: parames, headers: headers };
+    return this.httpClient.get<TreeNode[]>(this.baseUrl + this.prefix + "/saveEditUserDetails",options);
+  }
+  
+  createUser(postData: UserRoleFormData): Observable<TreeNode[]> {
+    return this.httpClient.post<TreeNode[]>(this.baseUrl + this.prefix + "/createUser", postData,
+      {
+        headers: appHeaders.getHeaders()
+      });
+  }
+  delete(userId,networkLoginId,pageNumber,totalPageCount,pageSize): Observable<TreeNode[]> {
+    let headers = appHeaders.getHeaders();
+    let parames = new HttpParams()
+      .set('userId', userId)
+      .set('networkLoginId', networkLoginId)
+      .set('pageNumber',pageNumber)
+      .set('totalPageCount',totalPageCount)
+      .set('pageSize',pageSize);
+    const options = { params: parames, headers: headers };
+    return this.httpClient.get<TreeNode[]>(this.baseUrl + this.prefix + "/delete", options);
+  }
 }
