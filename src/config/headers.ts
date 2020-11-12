357af818659db93d351f8840e56b155852f1b51f
DinakarCiber <dnaramala@ciber.com>
2020-11-12 15:36:07 +0530

    change for bulk request

--- src/config/headers.ts
+++ src/config/headers.ts
@@ -14,7 +14,12 @@ export class appHeaders {
     public static getHeaders_formData() {
         return new HttpHeaders({
             //'Content-Type': 'multipart/form-data; charset=utf-8',
-            'Content-Type': 'multipart/form-data;',
+            //'Content-Type': 'multipart/form-data;',
+            //'Content-Type': 'application/json; charset=utf-8',
+            //'Content-Type':'multipart/mixed;boundary=gc0p4Jq0M2Yt08jU534c0p',
+            'Content-Type':'multipart/form-data;boundary=gc0p4Jq0M2Yt08jU534c0p',
+            'enctype' : 'multipart/form-data',
+            //'Content-Type': 'false',
             'Accept': 'application/json',
             'api_key':'recap'
         });
