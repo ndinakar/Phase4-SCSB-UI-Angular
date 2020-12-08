b69287ee14952dc73c88754d0716ae08aa148cea
DinakarCiber <dnaramala@ciber.com>
2020-12-08 12:01:33 +0530

    changes for urls

--- src/config/urls.ts
+++ src/config/urls.ts
@@ -8,8 +8,9 @@ export const urls = {
     jobs: '/jobs',
     userRoles:'/userRoles',
     roles: '/roles',
-    bulkRequest: '/bulkRequest',
+    bulkRequest: '/bulkRequest',    
     admin:'/admin',
+    login:'/login',
     ui_url:'http://52.203.96.232:9091',
     openMarc:'/openMarcRecord',
     batchScheduleUrl:'http://52.203.96.232:9096'
