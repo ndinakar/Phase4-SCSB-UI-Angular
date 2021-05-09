31c0f5bef29044358645946e024799060f841e6c
DinakarCiber <dnaramala@ciber.com>
2021-05-09 17:43:14 +0530

    changes for User's Id

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { urls } from '@config/urls';
 import { environment } from 'src/environments/environment';
+import { RolesPermissionsService } from '@service/rolesPermissions/roles-permissions.service';
 
 @Component({
   selector: 'app-header',
@@ -11,16 +12,21 @@ import { environment } from 'src/environments/environment';
 export class HeaderComponent implements OnInit {
   LOGOUT = urls.LOGOUT;
   userName: string;
+  userDesc: string;
   tempUserName: string;
   url: string = '';
-  constructor(private cookieService: CookieService) { }
+  rolesRes: Object;
+  constructor(private cookieService: CookieService,private rolesService: RolesPermissionsService) { }
 
   ngOnInit(): void {
     this.url = environment.homeUrl + this.LOGOUT + this.cookieService.get('CSRF-TOKEN');
+    this.rolesRes = this.rolesService.getRes();
     this.tempUserName = this.cookieService.get('userName');
     if (this.tempUserName) {
       localStorage.setItem("userName", this.tempUserName);
+      localStorage.setItem("userDesc", this.rolesRes['userDesc']);
       this.userName = localStorage.getItem("userName");
+      this.userDesc = localStorage.getItem("userDesc");
     }
   }
 }
