e5a28c494c2c7bd318e2cb2e881d61b4cd455b7d
DinakarCiber <dnaramala@ciber.com>
2021-05-24 02:30:26 +0530

    refactor

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -3,20 +3,25 @@ import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
 import { DashBoardService } from './dashBoard/dash-board.service';
+
+enum CONSTANTS {
+    API_PATH = 'api_path'
+}
+
 @Injectable({
     providedIn: 'root'
 })
 export class InterceptorService implements HttpInterceptor {
-    constructor(private dashBoardService : DashBoardService) { }
+    constructor(private dashBoardService: DashBoardService) { }
     handleError(error: HttpErrorResponse) {
         return throwError(error);
     }
     intercept(req: HttpRequest<any>, next: HttpHandler):
         Observable<HttpEvent<any>> {
-            const authReq = req.clone({
-                headers: req.headers
-                    .set('api_path', 'search')
-            });
+        const authReq = req.clone({
+            headers: req.headers
+                .set(CONSTANTS.API_PATH, 'search')
+        });
         return next.handle(authReq)
             .pipe(
                 tap(evt => {
