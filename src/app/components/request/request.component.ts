2cd2b1bf0cae1560bf22d148083c07109038a342
ndinakar <dinkarn31@gmail.com>
2022-10-10 11:09:36 +0530

    changes for refactor

--- src/app/components/request/request.component.ts
+++ src/app/components/request/request.component.ts
@@ -510,9 +510,8 @@ export class RequestComponent implements OnInit,OnDestroy {
         for(var i = 0; i < this.deliveryLocVal.length; i++) {
           var location = this.deliveryLocVal[i];
           if(location[0] == this.deliveryLocationId){
-            this.deliveryLocationId = location[1]+"-"+location[0];
+            this.deliveryLocationId =location[0];
             isHaving = true;
-            console.log("new delivery location::"+this.deliveryLocationId);
           }
         }
         if(!isHaving){
@@ -847,21 +846,9 @@ export class RequestComponent implements OnInit,OnDestroy {
     this.initialload();
   }
   loadCreateRequestForSamePatron(patronId, reqInstId ,emailId, deliveryLocationId) {
-  /*  this.eddshow = false;
+    this.eddshow = false;
     this.createsubmit = false;
-    var isHaving = false;
-    console.log("old delivery location::"+this.deliveryLocationId);
-    for(var i = 0; i < this.deliveryLocVal.length; i++) {
-      var location = this.deliveryLocVal[i];
-      if(location[0] == this.deliveryLocationId){
-        this.deliveryLocationId = location[1]+"-"+location[0];
-        isHaving = true;
-        console.log("new delivery location::"+this.deliveryLocationId);
-      }
-    }
-    if(!isHaving){
-      this.deliveryLocationId='';
-    }*/
+
     this.requestService.loadCreateRequest().subscribe(
       (res) => {
         this.requestVal = res;
